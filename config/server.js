18e5257d7a60432f81fd2c0aefc66f23ea3d4175
SehatQ <sehatq@SehatQs-MacBook-Pro.local>
2021-01-12 11:00:23 +0700

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8778f177b5c144d2c261cd64a9603d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17c763d0-05d1-4ed2-9eb9-a90018add47f'
+};
